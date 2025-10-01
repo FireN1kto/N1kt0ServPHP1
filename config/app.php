4563127960fa93fb41c58375345d9512d76fb307
F1reN1kto <maksimcymbal77@gmail.com>
2025-10-02 00:09:31 +0700

    убрал защиту от CSRF-атак

--- config/app.php
+++ config/app.php
@@ -12,7 +12,6 @@ return [
         'unique' => \Validators\UniqueValidator::class
     ],
     'routeAppMiddleware' => [
-        'csrf' => \Middlewares\CSRFMiddleware::class,
         'trim' => \Middlewares\TrimMiddleware::class,
         'specialChars' => \Middlewares\SpecialCharsMiddleware::class
     ],
--- views/site/signup.php
+++ views/site/signup.php
@@ -1,9 +1,24 @@
 <h2>Регистрация нового пользователя</h2>
-<pre><?= $message ?? ''; ?></pre>
+<h3 class="errorValidate"><?= $message ?? ''; ?></h3>
 <form method="post">
-    <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
     <label>Имя <input type="text" name="name"></label>
     <label>Логин <input type="text" name="login"></label>
     <label>Пароль <input type="password" name="password"></label>
-    <button>Зарегистрироваться</button>
+    <div>
+        <label>Роль:</label>
+        <select id="role_id" name="role_id" <?= !$allowAdmin ? "disabled" : ""; ?>>
+            <?php foreach ($roles as $role): ?>
+                <?php if ($role->name_role === 'admin' && !$allowAdmin) continue ?>
+                <?php if ($role->name_role === 'registration_officer') continue; ?>
+                <option value="<?= $role->id ?>" <?= ($role->name_role === 'user' && !$allowAdmin) ? "selected" : ""?>>
+                    <?= match($role->name_role) {
+                        'admin' => 'Администратор',
+                        'user' => 'Посетитель',
+                        default => $role->name_role
+                    } ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <button>Регистрация</button>
 </form>
